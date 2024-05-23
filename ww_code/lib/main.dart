446e64a8afc999cc5d032c879d00b3c86335517f
Matt <e1155311@u.nus.edu>
2024-05-24 00:08:22 +0800

    added: firebase dependencies & package, established connection to flutter project

--- ww_code/lib/main.dart
+++ ww_code/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'menu_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   _setupLogging();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
