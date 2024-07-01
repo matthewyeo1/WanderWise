0b5d902a1eb62a40a0b732eae291556d85c84490
Matt <e1155311@u.nus.edu>
2024-07-01 17:54:37 +0800

    -added: input boolean statements for validity

--- ww_code/lib/ai_itinerary_page.dart
+++ ww_code/lib/ai_itinerary_page.dart
@@ -4,23 +4,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'aesthetics/textfield_style.dart';
+import 'utilities/utils.dart';
 
 class GeminiChatPage extends StatefulWidget {
   const GeminiChatPage({super.key});
   @override
-  _GeminiChatPageState createState() => _GeminiChatPageState();
+  GeminiChatPageState createState() => GeminiChatPageState();
 }
 
-class _GeminiChatPageState extends State<GeminiChatPage> {
+class GeminiChatPageState extends State<GeminiChatPage> {
   final Gemini gemini = Gemini.instance; // Answer from Gemini
   List<ChatMessage> _messages = []; // List to hold chat messages
   late String userId; // Variable to hold current user ID
-  TextEditingController textController = TextEditingController();
+  TextEditingController budgetController = TextEditingController();
+  TextEditingController destinationController = TextEditingController();
+  TextEditingController durationController = TextEditingController();
   bool isDarkMode = false;
 
   ChatUser geminiUser = ChatUser(
     id: '0',
-    firstName: 'WanderWise',
   );
 
   @override
@@ -62,36 +64,71 @@ class _GeminiChatPageState extends State<GeminiChatPage> {
           Expanded(
             child: DashChat(
               messages: _messages,
-              onSend: _handleSendMessage,
+              onSend: (ChatMessage chatMessage) {},
               currentUser: ChatUser(
                 id: userId,
                 firstName: userId,
               ),
-              inputOptions: InputOptions(
-                inputTextStyle: const TextStyle(color: Colors.black),
-                cursorStyle: const CursorStyle(color: Colors.black),
-                sendButtonBuilder: (sendMessage) {
-                  return IconButton(
-                    icon: const Icon(Icons.send),
-                    onPressed: () {
-                      sendMessage();
-                    },
-                  );
-                },
-                inputDecoration: TextFieldConfig.buildInputDecoration(
-                  hintText: 'Type a message...',
-                  prefixIcon: const Icon(
-                    Icons.message,
-                    color: Colors.black45,
+              inputOptions: const InputOptions(
+                  inputDisabled: true, 
+                  inputDecoration: InputDecoration()
+                ),
+                messageOptions: MessageOptions(
+                  currentUserContainerColor: !isDarkMode ? Colors.lightBlue : const Color(0xFF191970),
+                  currentUserTextColor: !isDarkMode ? Colors.white : Colors.white 
+                ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  style: const TextStyle(color: Colors.black),
+                  cursorColor: Colors.black,
+                  controller: budgetController,
+                  decoration: TextFieldConfig.buildInputDecoration(
+                    hintText: 'Enter budget',
+                    prefixIcon: const Icon(
+                      Icons.attach_money,
+                      color: Colors.black45,
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(height: 5),
+                TextField(
+                  style: const TextStyle(color: Colors.black),
+                  cursorColor: Colors.black,
+                  controller: destinationController,
+                  decoration: TextFieldConfig.buildInputDecoration(
+                    hintText: 'Enter destination',
+                    prefixIcon: const Icon(
+                      Icons.location_on,
+                      color: Colors.black45,
+                    ),
                   ),
                 ),
-                textController: textController,
-              ),
-              messageOptions: MessageOptions(
-                  currentUserContainerColor:
-                      !isDarkMode ? Colors.blue : Colors.white,
-                  currentUserTextColor:
-                      !isDarkMode ? Colors.white : Colors.black),
+                const SizedBox(height: 5),
+                TextField(
+                  style: const TextStyle(color: Colors.black),
+                  cursorColor: Colors.black,
+                  controller: durationController,
+                  decoration: TextFieldConfig.buildInputDecoration(
+                    hintText: 'Enter duration (days)',
+                    prefixIcon: const Icon(
+                      Icons.calendar_today,
+                      color: Colors.black45,
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: _handleSendMessage,
+                  child: const Text('Send'),
+                ),
+              ],
             ),
           ),
         ],
@@ -99,30 +136,89 @@ class _GeminiChatPageState extends State<GeminiChatPage> {
     );
   }
 
-  // Handle sending messages
-  void _handleSendMessage(ChatMessage chatMessage) {
+  void _handleSendMessage() {
+    String budgetStr = budgetController.text;
+    String destination = destinationController.text;
+    String durationStr = durationController.text;
+
+    if (budgetStr.isEmpty || destination.isEmpty || durationStr.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please fill all the fields'),
+        ),
+      );
+      return;
+    }
+
+    int budget;
+    int duration;
+
+    budget = int.parse(budgetStr);
+    duration = int.parse(durationStr);
+
+    if (!isValidBudget(budget)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Budget must be at least \$100"),
+        ),
+      );
+      return;
+    }
+
+    if (!isValidDestination(destination)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Please enter a valid destination"),
+        ),
+      );
+      return;
+    }
+
+    if (!isValidDuration(duration)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Your duration of stay should be between 1 to 30 days"),
+        ),
+      );
+      return;
+    }
+
+    String prompt =
+        'Create an itinerary to $destination, with a budget of \$$budget for $duration days.';
+
+    ChatMessage chatMessage = ChatMessage(
+      user: ChatUser(
+        id: userId,
+        firstName: userId,
+      ),
+      createdAt: DateTime.now(),
+      text: prompt,
+    );
+
     setState(() {
-      _messages = [chatMessage, ..._messages]; // Add new message to the list
+      _messages = [chatMessage, ..._messages]; 
     });
 
     try {
-      String response = chatMessage.text;
-      gemini.streamGenerateContent(response).listen((event) {
+      gemini.streamGenerateContent(prompt).listen((event) {
         ChatMessage? lastMessage = _messages.firstOrNull;
         if (lastMessage != null && lastMessage.user == geminiUser) {
           lastMessage = _messages.removeAt(0);
-          String response = event.content?.parts?.fold("", (previous, current) => "$previous ${current.text}") ?? "";
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
           lastMessage.text += response;
           setState(() {
             _messages = [lastMessage!, ..._messages];
           });
-
         } else {
-          String response = event.content?.parts?.fold("", (previous, current) => "$previous ${current.text}") ?? "";
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
           ChatMessage message = ChatMessage(
-            user: geminiUser, 
+            user: geminiUser,
             createdAt: DateTime.now(),
-             text: response,
+            text: response,
           );
           setState(() {
             _messages = [message, ..._messages];
@@ -132,7 +228,7 @@ class _GeminiChatPageState extends State<GeminiChatPage> {
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('An error occured: $e'),
+          content: Text('An error occurred: $e'),
         ),
       );
     }
